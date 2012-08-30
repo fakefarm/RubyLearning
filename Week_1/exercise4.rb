b54099581b705ea6742e269aafe06f3a9e91b855
Dave Woodall <dmwoodall@gmail.com>
2012-08-30 15:29:52 -0600

    back on Ruby Learning!

---
+++ Week_1/exercise4.rb
@@ -0,0 +1,4 @@
+# Exercise4. Write a Ruby program that tells you how many minutes there are in a year (do not bother right now about leap years etc.).
+
+MINUTES_IN_YEAR = 365 * 24 * 60
+puts MINUTES_IN_YEAR
