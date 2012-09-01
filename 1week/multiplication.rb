b2f5f0dbec6cdffe7ab5d12427fe02fb2dcc7263
Victor Goff <keeperotphones@gmail.com>
2012-08-31 19:15:00 -0500

    Moved title and docoration tests above those two methods.  See extended
    Fixed alignment of your large comment block, it looks like you may have
    used a linter of some kind.  (Code beautifier) and it broke the
    comments.

--- 1week/multiplication.rb
+++ 1week/multiplication.rb
@@ -1,42 +1,44 @@
 
 =begin
-    doctest: Setup times table 9 proof variable
-    >> times_table_9_proof = " Times Table to 9\n ===========================\n 1 2 3 4 5 6 7 8 9\n 2 4 6 8 10 12 14 16 18\n 3 6 9 12 15 18 21 24 27\n 4 8 12 16 20 24 28 32 36\n 5 10 15 20 25 30 35 40 45\n 6 12 18 24 30 36 42 48 54\n 7 14 21 28 35 42 49 56 63\n 8 16 24 32 40 48 56 64 72\n 9 18 27 36 45 54 63 72 81\n ===========================\n"
+doctest: Setup times table 9 proof variable
+>> times_table_9_proof = " Times Table to 9\n ===========================\n 1 2 3 4 5 6 7 8 9\n 2 4 6 8 10 12 14 16 18\n 3 6 9 12 15 18 21 24 27\n 4 8 12 16 20 24 28 32 36\n 5 10 15 20 25 30 35 40 45\n 6 12 18 24 30 36 42 48 54\n 7 14 21 28 35 42 49 56 63\n 8 16 24 32 40 48 56 64 72\n 9 18 27 36 45 54 63 72 81\n ===========================\n"
 
+doctest: Testing times table 9 with title and decoration
 
-    doctest: Testing times table 9 with title and decoration
+>> times_table_9_proof = <<-EOS 
+ Times Table to 9
+ ===========================
+  1  2  3  4  5  6  7  8  9
+  2  4  6  8 10 12 14 16 18
+  3  6  9 12 15 18 21 24 27
+  4  8 12 16 20 24 28 32 36
+  5 10 15 20 25 30 35 40 45
+  6 12 18 24 30 36 42 48 54
+  7 14 21 28 35 42 49 56 63
+  8 16 24 32 40 48 56 64 72
+  9 18 27 36 45 54 63 72 81
+ ===========================
+ EOS
 
-    times_table_9_proof = <<-EOS 
-     Times Table to 9
-     ===========================
-      1  2  3  4  5  6  7  8  9
-      2  4  6  8 10 12 14 16 18
-      3  6  9 12 15 18 21 24 27
-      4  8 12 16 20 24 28 32 36
-      5 10 15 20 25 30 35 40 45
-      6 12 18 24 30 36 42 48 54
-      7 14 21 28 35 42 49 56 63
-      8 16 24 32 40 48 56 64 72
-      9 18 27 36 45 54 63 72 81
-     ===========================
-     EOS
-
-    >> multiplication_table(9, 'Times Table to 9', true) == times_table_9_proof
-    => true
-
-    doctest: set_title method returns the title
-    >> set_title("title")
-    => "title"
-
-    doctest: decoration method set to true will provide fancy equal bars "="
-    >> decoration(10, true)
-    => "=========="
+>> multiplication_table(9, 'Times Table to 9', true) == times_table_9_proof
+=> true
+    
 =end
 
+=begin
+doctest: set_title method returns the title
+>> set_title("title")
+=> "title"
+=end
 def set_title(title)
   puts "#{title}"
 end
 
+=begin
+doctest: decoration method set to true will provide fancy equal bars "="
+>> decoration(10, true)
+=> "=========="
+=end
 def decoration(count, switch = true)
   puts "".center(count,"=") if switch == true
 end
